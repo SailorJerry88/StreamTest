ecf69c3e7fa1a2e6696f6a50f7698505a034c9ed
Sailor Jerry <michalmycek@protonmail.com>
2019-11-23 10:24:30 +0100

    Stream22

--- lesson3/MainStrem.java
+++ lesson3/MainStrem.java
@@ -1,28 +1,24 @@
 package pl.sda.rafal.zientara.game.lesson3;
 
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MainStrem {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(2,4);
-
-        numbers.stream()
-                .map(new Function<Integer, Double>() {
-                @Override
-                    public Double apply(Integer integer){
-                    return integer / 2.; // 0.5
-                    
-                    // return integer as (.)
-                    // 2.
-                }
+        List<Integer> numbers = Arrays.asList(2,4,5);
 
 
 
-                })
+        List<Integer> output = numbers.stream() Stream<Integer>
+                .map(integer -> integer / 2.) Stream<Double>
+                .map(aDouble -> aDouble * 2) Stream<Double>
+                .filter(filtr -> filtr > 4)
                 .collect(Collectors.toList());
+        System.out.println(output);
     }
 }
